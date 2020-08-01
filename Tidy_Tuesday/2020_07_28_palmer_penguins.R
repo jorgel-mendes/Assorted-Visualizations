c8878e8026eafeab8ce8d6e0ba81b7a02d8c6623
Jorge Mendes <jorgeluis_sm18@hotmail.com>
2020-08-01 12:06:13 -0300

    Tidy Tuesday Palmer Penguins

---
+++ Tidy_Tuesday/2020_07_28_palmer_penguins.R
@@ -0,0 +1,28 @@
+library(tidyverse)
+library(waffle)
+
+penguins <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-07-28/penguins.csv')
+
+glimpse(penguins)
+
+penguins %>% 
+  group_by(species) %>% 
+  count(island) %>% 
+  ungroup() %>% 
+  mutate(species = fct_relevel(species, c('Adelie', 'Gentoo', 'Chinstrap'))) %>% 
+  ggplot(aes(fill = species, values = n)) +
+  geom_waffle(color = "white", size = .2, n_rows = 10, flip = TRUE) +
+  facet_wrap(~island, nrow=1, strip.position = 'bottom') +
+  scale_x_discrete(expand=c(0,0)) +
+  scale_y_continuous(labels = function(x) x * 10, # make this multiplyer the same as n_rows
+                     expand = c(0,0)) +
+  scale_fill_manual(values = c('darkorange2', 'darkseagreen4', 'darkviolet')) +
+  labs(
+    title = "Penguin species proportion by island, Palmer Station LTER",
+    fill = "Species",
+    caption = 'By @jorgelsm for #TidyTuesday'
+  ) +
+  theme_minimal() +
+  theme(legend.position = 'top',
+        text = element_text(face = 'bold', color = 'grey20'),
+        strip.text = element_text(face = 'bold', color = 'grey20'))
