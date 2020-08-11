f412ae48f22b5eef50fc783effd5fcb03a620616
Jorge Mendes <jorgeluis_sm18@hotmail.com>
2020-08-11 17:11:40 -0300

    Tidy Tuesday Last Airbender

--- Tidy_Tuesday/2020_07_28_palmer_penguins.R
+++ Tidy_Tuesday/2020_07_28_palmer_penguins.R
@@ -1,3 +1,5 @@
+
+
 library(tidyverse)
 library(waffle)
 
@@ -14,15 +16,16 @@ penguins %>%
   geom_waffle(color = "white", size = .2, n_rows = 10, flip = TRUE) +
   facet_wrap(~island, nrow=1, strip.position = 'bottom') +
   scale_x_discrete(expand=c(0,0)) +
-  scale_y_continuous(labels = function(x) x * 10, # make this multiplyer the same as n_rows
+  scale_y_continuous(labels = function(x) x * 10,
                      expand = c(0,0)) +
   scale_fill_manual(values = c('darkorange2', 'darkseagreen4', 'darkviolet')) +
   labs(
-    title = "Penguin species proportion by island, Palmer Station LTER",
+    title = "Penguins per island, Palmer Station LTER",
     fill = "Species",
     caption = 'By @jorgelsm for #TidyTuesday'
   ) +
   theme_minimal() +
   theme(legend.position = 'top',
         text = element_text(face = 'bold', color = 'grey20'),
-        strip.text = element_text(face = 'bold', color = 'grey20'))
+        strip.text = element_text(face = 'bold', color = 'grey20'),
+        panel.grid = element_blank())
